eecae5c7ef158ac76d99e59341414fc72f1522e0
Iuri Matias <iuri.matias@gmail.com>
2025-10-08 15:39:04 -0400

    update flake so it compiles cpp-generator as well

--- flake.nix
+++ flake.nix
@@ -11,39 +11,81 @@
       });
     in
     {
-      packages = forAllSystems ({ pkgs }: {
-        default = pkgs.stdenv.mkDerivation rec {
-          pname = "logos-cpp-sdk";
-          version = "0.1.0";
-          
-          src = ./.;
-          
-          
-          nativeBuildInputs = [ 
-            pkgs.cmake 
-            pkgs.ninja 
-            pkgs.pkg-config
-            pkgs.qt6.wrapQtAppsNoGuiHook
-          ];
-          buildInputs = [ 
-            pkgs.qt6.qtbase 
-            pkgs.qt6.qtremoteobjects 
-          ];
-          
-          
-          cmakeFlags = [ "-GNinja" ];
-          
-          configurePhase = ''
-            cd cpp
-            cmakeConfigurePhase
-          '';
-          
-          meta = with pkgs.lib; {
-            description = "Logos C++ SDK";
-            platforms = platforms.unix;
+      packages = forAllSystems ({ pkgs }: 
+        let
+          sdk = pkgs.stdenv.mkDerivation rec {
+            pname = "logos-cpp-sdk";
+            version = "0.1.0";
+            
+            src = ./.;
+            
+            nativeBuildInputs = [ 
+              pkgs.cmake 
+              pkgs.ninja 
+              pkgs.pkg-config
+              pkgs.qt6.wrapQtAppsNoGuiHook
+            ];
+            buildInputs = [ 
+              pkgs.qt6.qtbase 
+              pkgs.qt6.qtremoteobjects 
+            ];
+            
+            cmakeFlags = [ "-GNinja" ];
+            
+            configurePhase = ''
+              cd cpp
+              cmakeConfigurePhase
+            '';
+            
+            meta = with pkgs.lib; {
+              description = "Logos C++ SDK Library";
+              platforms = platforms.unix;
+            };
           };
-        };
-      });
+
+          generator = pkgs.stdenv.mkDerivation rec {
+            pname = "logos-cpp-generator";
+            version = "0.1.0";
+            
+            src = ./.;
+            
+            nativeBuildInputs = [ 
+              pkgs.cmake 
+              pkgs.ninja 
+              pkgs.pkg-config
+              pkgs.qt6.wrapQtAppsNoGuiHook
+            ];
+            buildInputs = [ 
+              pkgs.qt6.qtbase 
+            ];
+            
+            cmakeFlags = [ "-GNinja" ];
+            
+            configurePhase = ''
+              cd cpp-generator
+              cmakeConfigurePhase
+            '';
+            
+            installPhase = ''
+              mkdir -p $out/bin
+              cp bin/logos-cpp-generator $out/bin/
+            '';
+            
+            meta = with pkgs.lib; {
+              description = "Logos C++ Code Generator";
+              platforms = platforms.unix;
+            };
+          };
+        in
+        {
+          logos-cpp-sdk = sdk;
+          cpp-generator = generator;
+          default = pkgs.symlinkJoin {
+            name = "logos-cpp-sdk-with-generator";
+            paths = [ sdk generator ];
+          };
+        }
+      );
 
       devShells = forAllSystems ({ pkgs }: {
         default = pkgs.mkShell {
